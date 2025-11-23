1d77466b5403ac11a764567a8d079f50a88eab2d
Austin Caron <austin.caron1@gmail.com>
2025-11-23 04:00:58 -0500

    renaming hero img

--- app/index.tsx
+++ app/index.tsx
@@ -55,7 +55,7 @@ export default function HomeScreen() {
       <View style={styles.hero}>
         <View style={styles.heroImageWrapper}>
           <Animated.Image
-            source={require('@/assets/images/heroImg-Christmas.png')}
+            source={require('@/assets/images/heroimg-christmas.png')}
             style={[styles.heroImage, imageAnimatedStyle]}
             resizeMode="cover"
             accessible
